54d0c2b70acbeef9cf98403fb2a051d23d45637b
Rafael de Souza Silva <77814194+RafaSS@users.noreply.github.com>
2024-11-11 17:31:48 -0300

    add card

--- src/views/PokemonDetailsView.vue
+++ src/views/PokemonDetailsView.vue
@@ -1,11 +1,9 @@
-
-
-
 <script setup lang="ts">
 import { useRoute } from 'vue-router';
-import { defineComponent, ref, onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
 import { fetchPokemonDetails } from '../api/pokemonApi';
 import type { PokemonDetails } from '../interfaces/Pokemon';
+
 const route = useRoute();
 const pokemonDetail = ref<PokemonDetails>();
 
@@ -16,12 +14,19 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div>
-    <h1>Pokemon Details</h1>
-    <p>{{ pokemonDetail?.name }}</p>
-    <p>{{ pokemonDetail?.height }}</p>
-    <p>{{ pokemonDetail?.weight }}</p>
+  <div class="max-w-md mx-auto p-5 border border-gray-300 rounded-lg shadow-md bg-red-500">
+    <h1 class="text-2xl font-bold mb-4">{{ pokemonDetail?.name }}</h1>
+    <div class="flex flex-col items-center">
+      <img :src="pokemonDetail?.sprites?.front_default" alt="Pokemon Image" class="mb-4">
+      <p><strong>Height:</strong> {{ pokemonDetail?.height }}</p>
+      <p><strong>Weight:</strong> {{ pokemonDetail?.weight }}</p>
+      <p><strong>Base Experience:</strong> {{ pokemonDetail?.base_experience }}</p>
+      <p><strong>Abilities:</strong></p>
+      <ul>
+        <li v-for="ability in pokemonDetail?.abilities" :key="ability.ability.name">
+          {{ ability.ability.name }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
-
-<style scoped></style>
