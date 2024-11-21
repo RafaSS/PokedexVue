cf07e59a846d4595accf0112bda6c86155a76176
Rafael de Souza Silva <77814194+RafaSS@users.noreply.github.com>
2024-11-21 01:57:57 -0300

    refactor PokemonDetailsView to improve evolution navigation; add previous and next evolution details

--- src/views/PokemonDetailsView.vue
+++ src/views/PokemonDetailsView.vue
@@ -16,21 +16,39 @@ import { usePokemonStore } from '../store/pokemon';
 const route = useRoute();
 const router = useRouter();
 const pokemonDetail = ref<PokemonDetails>();
-const pokemonEvotion = ref<EvolutionChain>();
+const pokemonEvolution = ref<EvolutionChain>();
 const pokemonSpecies = ref<PokemonSpecies>();
 const isFavorite = ref(false);
+const previousEvolutionDetail = ref<PokemonDetails>();
+const nextEvolutionDetail = ref<PokemonDetails>();
 
 const loadData = async () => {
   pokemonDetail.value = await fetchPokemonDetails(route.params.name as string);
   pokemonSpecies.value = await fetchPokemonSpecies(route.params.name as string);
-  pokemonEvotion.value = await fetchPokemonEvolutionChain(
+  pokemonEvolution.value = await fetchPokemonEvolutionChain(
     extractEvolutionIdFromUrl(
       pokemonSpecies.value?.evolution_chain.url as string,
     ),
   );
-  isFavorite.value = await usePokemonStore().isFavoritePokemon(
+  isFavorite.value = usePokemonStore().isFavoritePokemon(
     pokemonDetail.value.name as string,
   );
+
+  const previousSpecies = findPreviousEvolution(
+    pokemonEvolution.value?.chain,
+    route.params.name as string,
+  );
+  const nextSpecies = findNextEvolution(
+    pokemonEvolution.value?.chain,
+    route.params.name as string,
+  );
+
+  if (previousSpecies) {
+    previousEvolutionDetail.value = await fetchPokemonDetails(previousSpecies);
+  }
+  if (nextSpecies) {
+    nextEvolutionDetail.value = await fetchPokemonDetails(nextSpecies);
+  }
 };
 
 // Call loadData on component mount
@@ -41,43 +59,13 @@ watch(() => route.params.name, loadData);
 
 const goToEvolution = async (direction: 'next' | 'previous') => {
   if (direction === 'next') {
-    if (pokemonEvotion.value?.chain.evolves_to[0]) {
-      const nextSpecies = findNextEvolution(
-        pokemonEvotion.value?.chain,
-        route.params.name as string,
-      );
-      if (nextSpecies) {
-        router.push(
-          `/pokemondetail/${nextSpecies}`,
-        );
-      }
+    if (nextEvolutionDetail.value) {
+      router.push(`/pokemondetail/${nextEvolutionDetail.value.name}`);
     }
   } else if (direction === 'previous') {
-    const previousSpecies = findPreviousEvolution(
-      pokemonEvotion.value?.chain,
-      route.params.name as string,
-    );
-    if (previousSpecies) {
-      router.push(
-        `/pokemondetail/${previousSpecies}`,
-      );
+    if (previousEvolutionDetail.value) {
+      router.push(`/pokemondetail/${previousEvolutionDetail.value.name}`);
     }
-  }
-};
-
-const toggleFavorite = async () => {
-  isFavorite.value = !isFavorite.value;
-  if (isFavorite.value) {
-    if (pokemonDetail.value) {
-      usePokemonStore().saveFavoritePokemon({
-        name: pokemonDetail.value.name,
-        url: pokemonDetail.value.species.url,
-      });
-    }
-  } else {
-    usePokemonStore().removeFavoritePokemon(
-      pokemonDetail.value?.name as string,
-    );
   }
 };
 
@@ -119,40 +107,49 @@ const extractEvolutionIdFromUrl = (url: string): number => {
 </script>
 
 <template>
-
-  <div class="relative max-w-md mx-auto p-5 border border-gray-300 rounded-lg shadow-md bg-red-500">
-    <h1 class="text-2xl font-bold mb-4">{{ pokemonDetail?.name }}</h1>
-    <div class="absolute top-2 right-2" @click="toggleFavorite">
-      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6"
-        :class="{ 'text-yellow-400': isFavorite, 'text-gray-400': !isFavorite }" fill="currentColor" viewBox="0 0 24 24"
-        stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-          d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.286 3.957a1 1 0 00.95.69h4.162c.969 0 1.371 1.24.588 1.81l-3.37 2.455a1 1 0 00-.364 1.118l1.286 3.957c.3.921-.755 1.688-1.54 1.118l-3.37-2.455a1 1 0 00-1.175 0l-3.37 2.455c-.784.57-1.838-.197-1.54-1.118l1.286-3.957a1 1 0 00-.364-1.118L2.98 9.384c-.783-.57-.38-1.81.588-1.81h4.162a1 1 0 00.95-.69l1.286-3.957z" />
-      </svg>
-    </div>
-    <div class="flex flex-col items-center">
-      <img :src="pokemonDetail?.sprites?.front_default" alt="Pokemon Image" class="mb-4" />
-      <p><strong>Height:</strong> {{ pokemonDetail?.height }}</p>
-      <p><strong>Weight:</strong> {{ pokemonDetail?.weight }}</p>
-      <p>
-        <strong>Base Experience:</strong> {{ pokemonDetail?.base_experience }}
-      </p>
-      <p><strong>Abilities:</strong></p>
-      <ul>
-        <li v-for="ability in pokemonDetail?.abilities" :key="ability.ability.name">
-          {{ ability.ability.name }}
-        </li>
-      </ul>
-      <div class="flex space-x-2">
-        <button v-if="pokemonDetail?.species?.url" @click="goToEvolution('previous')"
-          class="mt-4 px-4 py-2 bg-blue-500 text-white hover:bg-blue-600 focus:outline-none">
-          ← Previous Evolution
-        </button>
-        <button v-if="pokemonDetail?.species?.url" @click="goToEvolution('next')"
-          class="mt-4 px-4 py-2 bg-blue-500 text-white hover:bg-blue-600 focus:outline-none">
-          Go to Evolution →
-        </button>
+  <div class="Frame3 w-[1920px] h-[1080px] relative bg-white">
+    <div
+      class="Card w-[1678px] h-[547px] p-6 left-[142px] top-[102px] absolute bg-[#3085c6] rounded-[27px] border border-[#d9d9d9] justify-start items-start gap-6 inline-flex">
+      <div class="Body grow shrink basis-0 h-[374px] flex-col justify-start items-start gap-4 inline-flex">
+        <div class="Marill self-stretch text-neutral-100 text-7xl font-bold font-['Inter'] leading-[86.40px]">
+          {{ pokemonDetail?.name }}
+        </div>
+        <div class="Frame2 w-96 grid grid-cols-2 gap-4">
+          <div v-for="stat in pokemonDetail?.stats" :key="stat.stat.name" class="col-span-1">
+            <div class="Hp left-0 top-0 text-black text-2xl font-semibold font-['Inter'] leading-[28.80px]">
+              {{ stat.stat.name }}
+              <svg xmlns="http://www.w3.org/2000/svg" width="138" height="7" viewBox="0 0 138 7" fill="none">
+                <path d="M3 4H135" stroke="#949494" stroke-width="6" stroke-linecap="round" />
+                <path :d="'M3 3.97861L' + (3 + stat.base_stat) + ' 3.9998'" stroke="white" stroke-width="6"
+                  stroke-linecap="round" />
+              </svg>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
+
+  <img class="Marill w-[1006px] h-[795px] left-[600px] top-[200px] absolute "
+    :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonDetail?.id}.svg`" />
+
+  <div class="Group1 w-[132px] h-[153px] left-[142px] top-[784px] absolute">
+    <img class="Image1 w-[90px] h-[81px] left-[21px] top-0 absolute shadow"
+      :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${previousEvolutionDetail?.id}.svg`" />
+    <div
+      class="Button w-[132px] h-[52.47px] p-3 left-0 top-[100.53px] absolute bg-[#185fa0] rounded-lg justify-center items-center gap-2 inline-flex"
+      @click="goToEvolution('previous')">
+      <div class="Button text-neutral-100 text-base font-normal font-['Inter'] leading-none">
+        < </div>
+      </div>
+    </div>
+    <div class="Group2 w-[134px] h-[170px] left-[1686px] top-[767px] absolute">
+      <img class="1 w-[114px] h-[116px] left-[3px] top-0 absolute shadow"
+        :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${nextEvolutionDetail?.id}.svg`" />
+      <div
+        class="Button w-[134px] h-[51px] p-3 left-0 top-[119px] absolute bg-[#175ea0] rounded-lg justify-center items-center gap-2 inline-flex"
+        @click="goToEvolution('next')">
+        <div class="Button text-neutral-100 text-base font-normal font-['Inter'] leading-none">></div>
+      </div>
+    </div>
 </template>
